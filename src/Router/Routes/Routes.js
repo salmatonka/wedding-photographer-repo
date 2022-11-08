5824819b8331318935ddc7e0d4a765e4e4d0b1ea
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-09 00:08:29 +0600

    fifth commit

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,9 @@
 import {createBrowserRouter} from 'react-router-dom'
 import Main from '../../layout/Main'
+import Blog from '../../Pages/Home/Blog/Blog';
 import Home from '../../Pages/Home/Home/Home';
+import Service from '../../Pages/Home/Service/Service';
+import Login from '../../Pages/Login/Login';
 const  router = createBrowserRouter([
     {
         path:'/',
@@ -9,7 +12,24 @@ const  router = createBrowserRouter([
             {
                 path:'/',
                 element:<Home></Home>
+            },
+            {
+                path:'/',
+                element:<Service></Service>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            },
+            {
+                path:'/register',
+                element:<Login></Login>
+            },
+            {
+                path:'/blog',
+                element:<Blog></Blog>
             }
+            
         ]
     }
 ])
