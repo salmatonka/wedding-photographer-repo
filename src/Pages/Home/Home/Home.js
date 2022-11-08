8558596d1a19673bd37298182099e163edb47c74
salmatonka <108433658+salmatonka@users.noreply.github.com>
2022-11-08 21:32:08 +0600

    third commit

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -10,7 +10,8 @@ const Home = () => {
         <div>
             <Slider></Slider>
             <Service></Service>
-           
+            <Elopement></Elopement>
+            
         </div>
     );
 };
